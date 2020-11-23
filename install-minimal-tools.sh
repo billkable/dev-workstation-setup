5b2ab42f66be24dac9cdea5f94ca9095c66c53df
Bill Kable <bkable@gmail.com>
2020-11-23 14:10:42 -0600

    debug

--- install-minimal-tools.sh
+++ install-minimal-tools.sh
@@ -45,4 +45,3 @@ sudo microk8s enable rbac dns storage
 sudo microk8s config > ~/.kube/config
 sudo usermod -aG microk8s ubuntu
 sudo chown -fR ubuntu ~/.kube
-kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
