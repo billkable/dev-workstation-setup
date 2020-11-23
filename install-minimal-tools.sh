a29722b9aa5227dfc663fec117541c84d3610769
Bill Kable <bkable@gmail.com>
2020-11-23 13:37:45 -0600

    make oh my zsh install non-interactive

--- install-minimal-tools.sh
+++ install-minimal-tools.sh
@@ -1,17 +1,4 @@
-
 #!/bin/bash
-
-# Configure desktop
-
-sudo apt update
-sudo apt upgrade -y
-
-sudo apt install openjdk-11-jdk-headless jq curl wget xclip -y
-sudo apt install gnome-session gnome-terminal gnome-tweaks -y
-sudo apt install ubuntu-desktop-minimal -y
-sudo apt install zsh -y
-
-# Packages and snaps
 
 sudo apt install \
     apt-transport-https \
@@ -27,11 +14,16 @@ sudo add-apt-repository \
    $(lsb_release -cs) \
    stable"
 
-sudo apt install docker-ce docker-ce-cli containerd.io -y
-
-sudo apt update -y
+sudo apt update
 sudo apt upgrade -y
 
+sudo apt install openjdk-11-jdk-headless jq curl wget xclip -y
+sudo apt install gnome-session gnome-terminal gnome-tweaks -y
+sudo apt install ubuntu-desktop-minimal -y
+sudo apt install docker-ce docker-ce-cli containerd.io -y
+sudo apt install zsh -y
+
+# Snaps
 sudo snap install --classic microk8s
 sudo snap install --classic kubectl
 sudo snap install --classic code
@@ -39,24 +31,22 @@ sudo snap install --classic postman
 sudo snap install --classic intellij-idea-ultimate
 
 # Configure docker
-
 sudo groupadd docker
 sudo usermod -aG docker ubuntu
 
 # Configure microk8s
-
 mkdir ~/.kube
-sudo microk8s enable ingress storage
+sudo microk8s enable rbac dns storage
 sudo microk8s config > ~/.kube/config
 sudo usermod -aG microk8s ubuntu
 sudo chown -fR ubuntu ~/.kube
+kubectl apply -f https://projectcontour.io/quickstart/contour.yaml
 
 # Docker Compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 # Octant
-
 wget https://github.com/vmware-tanzu/octant/releases/download/v0.14.1/octant_0.14.1_Linux-64bit.deb
 sudo apt install ./octant_0.14.1_Linux-64bit.deb
 rm octant_0.14.1_Linux-64bit.deb
@@ -73,14 +63,16 @@ alias k=kubectl
 complete -F __start_kubectl k
 
 # install zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh
+chmod +x ./install.sh
+CHSH=yes RUNZSH=no ./install.sh
+rm ./install.sh
 
 # Kubectl zsh alias
 source <(kubectl completion zsh)  # setup autocomplete in zsh into the current shell
 echo "source <(kubectl completion zsh)" >> ~/.zshrc # add autocomplete permanently to your zsh shell
 
 # Git alias
-
 git config --global alias.lola "log --graph --decorate --pretty=oneline --abbrev-commit --all"
 
 reboot
